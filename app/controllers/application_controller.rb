6964a2ddaa8ae9c0c3133a330d5afe9fba5f8623
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-14 16:14:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,9 +40,9 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
       if !!user
         session[:user_id] = user.id
-        redirect '/account'
+        redirect :account
       else
-        redirect '/failure'
+        redirect :failure
       end
   end
 
