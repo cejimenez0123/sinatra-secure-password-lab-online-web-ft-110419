00701f8f0051d11172eee498d903e1785159f5e6
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-14 16:09:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,9 +38,10 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(username: params[:username])
+    binding.pry
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        binding.pry
+        
         redirect :account
       else
         redirect :failure
